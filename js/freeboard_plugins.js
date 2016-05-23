2e6b349c2c707fbb71cfb44fa65c6e822ee125d7
sam gunaratne <samgzeit@gmail.com>
2016-05-23 13:01:12 +0100

    very basic save dashboard functionality

--- js/freeboard_plugins.js
+++ js/freeboard_plugins.js
@@ -5116,3 +5116,33 @@ freeboard.loadDatasourcePlugin({
     });
 
 }());
+
+var persistDashboard = function(_thisref, event)
+{
+  var username = 'radiator';
+  var password = prompt('Enter radiator password', '');
+  var basicAuth = 'Basic ' + btoa(username +':' + password);
+
+  var dashboard = freeboard.serialize();
+  fetch('/board/dashboard.json', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': basicAuth
+    },
+    body: JSON.stringify(dashboard)
+  })
+  .then(function(result) {
+    console.log(result);
+    if(result.ok) {
+      alert('Save Complete');
+    } else {
+      throw result.status;
+    }
+  })
+  .catch(function(err){
+    var err = JSON.stringify(err);
+    console.log(err);
+    alert(err);
+  });
+}
